3b7fea176f4df1b6e11b0b6823789f4cda1069ea
mamat91 <mathieu-le.barbier@hotmail.fr>
2020-03-07 18:31:05 +0100

    ft print numbers 2nd attempt

---
+++ day_02/ft_print_numbers.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ft_print_numbers.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/07/2020 03:39:31 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+
+void ft_print_numbers(void)
+{
+    char n = '0';
+
+    for ( n='0'; n <= '9'; n++)
+
+        putchar(n);
+
+}
