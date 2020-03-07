efae7a156e92198a6ea5263dd7a9332e74dc15a7
mamat91 <mathieu-le.barbier@hotmail.fr>
2020-03-07 17:02:42 +0100

    print alphabet and reverse 2nd attempt

---
+++ day_02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ft_print_alphabet.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/19/2020 05:04:26 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+
+    void ft_print_alphabet(void)
+    {
+      char ch ;
+      
+      for (ch = 'z'; ch >= 'a'; ch--)
+
+
+      putchar(ch);
+
+    }
