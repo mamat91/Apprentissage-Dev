460472aa29acb497ad1a4c400976798fce264e7d
mamat91 <mathieu-le.barbier@hotmail.fr>
2020-03-07 18:42:36 +0100

    ft is negative 1st attempt

---
+++ day_02/ft_is_negative.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ft_is_negative.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/07/2020 06:34:08 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+void ft_is_negative(int n)
+{
+    if (n<0)
+        putchar('N');
+    else
+        putchar('P');
+
+}
+
